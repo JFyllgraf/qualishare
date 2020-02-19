7b67d2dbe4cc02f9f1deef669776f4c19aac7c2c
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-02-19 14:42:20 +0100

    initial commit

---
+++ client/src/components/Join/Join.js
@@ -0,0 +1,24 @@
+import React, {useState } from 'react';
+import { Link } from 'react-router-dom';
+
+import './Join.css';
+
+const Join = () => {
+  const [name, setName] = useState('');
+  const [room, setRoom] = useState('');
+
+  return(
+    <div className="joinOuterContainer">
+      <div className="joinInnerContainer">
+        <h1 className="heading">Join</h1>
+        <div><input onChange={(event) => setName(event.target.value)} placeholder="Name" className="joinInput" type="text"/></div>
+        <div><input onChange={(event) => setRoom(event.target.value)} placeholder="Room" className="joinInput mt-20" type="text"/></div>
+        <Link onClick={event => (!name || !room) ? event.preventDefault() : null} to={`/chat?name=${name}&room=${room}`}>
+          <button className="button mt-20" type="submit">Sign In</button>
+        </Link>
+      </div>
+    </div>
+  )
+}
+
+export default Join
