be98d10126adfcc44e9ca0a9f5bc8410f51741d5
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-03-03 17:10:54 +0100

    started implementing css gid layout

--- client/src/App.js
+++ client/src/App.js
@@ -5,11 +5,28 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import Join from './components/Join/Join';
 import Chat from './components/Chat/Chat';
 
-const App = () => (
-    <Router>
-      <Route path="/" exact component={Join} />
-      <Route path="/chat" component={Chat} />
-    </Router>
-);
+import './App.css';
+
+const App = () => {
+
+
+  return(
+
+      <div>
+        <div className="container">
+            <div className="header">HEADER</div>
+            <div className="leftMenu">MENU1</div>
+            <div className="content">CONTENT</div>
+            <div className="rightMenu">MENU2</div>
+            <div className="footer">FOOTER</div>
+        </div>
+      </div>
+
+    // <Router>
+    //   <Route path="/" exact component={Join} />
+    //   <Route path="/chat" component={Chat} />
+    // </Router>
+  )
+};
 
 export default App;
