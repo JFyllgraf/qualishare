271815a181915d87d83c7b6c802eafa701bc9b79
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-03-05 15:30:25 +0100

    styling extra container; join and chat

--- client/src/components/Input/Input.js
+++ client/src/components/Input/Input.js
@@ -1,18 +1,21 @@
 import React from 'react';
 
+
 import './Input.css';
 
 const Input = ({ message, setMessage, sendMessage }) => (
   <form className="form">
-    <input
-    type="text"
+    <textarea
+    row="1"
     className="input"
     placeholder="Type a message..."
     value={message}
     onChange={(event) => setMessage(event.target.value)}
     onKeyPress={event => event.key === 'Enter' ? sendMessage(event) : null}
     />
-    <button className="sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+    <div className="btnContainer">
+      <button className="btn-lg sendButton" onClick={(event) => sendMessage(event)}>Send</button>
+    </div>
   </form>
 )
 
