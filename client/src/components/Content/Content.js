888f9bc6126b12f3d6ef012a94016e89b9386b42
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-03-09 14:35:10 +0100

    added feature to highlight text, and send through websocket. Not finished

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { Form, FormGroup, Label, Input } from 'reactstrap';
+import ContentEditable from 'react-contenteditable';
 import io from "socket.io-client";
 
 import './Content.css';
@@ -7,8 +8,7 @@ let socket;
 
 
 const Content = () => {
-  const initialText = "test 123";
-  const initialText2 = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur assumenda suscipit quos doloribus minus, provident corrupti repudiandae totam ipsam cum numquam! Repellat voluptas magnam amet, labore tempore laborum, dignissimos laudantium?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem error, nulla delectus id, nemo aliquam commodi, non distinctio pariatur nisi rem! Provident sapiente, natus assumenda cumque error, esse distinctio porro.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora optio debitis deleniti explicabo repellat quos ipsum itaque doloremque molestiae delectus a voluptates saepe vero iusto veritatis laudantium accusantium, assumenda sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quia enim, et assumenda odit rerum vero pariatur minus commodi iusto soluta architecto porro, cum ducimus id molestias odio vitae voluptates? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, libero numquam dolores temporibus exercitationem a voluptates sit minus perferendis iste consectetur accusamus pariatur tempore cupiditate adipisci labore corporis dolore eaque.";
+  const initialText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur assumenda suscipit quos doloribus minus, provident corrupti repudiandae totam ipsam cum numquam! Repellat voluptas magnam amet, labore tempore laborum, dignissimos laudantium?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem error, nulla delectus id, nemo aliquam commodi, non distinctio pariatur nisi rem! Provident sapiente, natus assumenda cumque error, esse distinctio porro.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora optio debitis deleniti explicabo repellat quos ipsum itaque doloremque molestiae delectus a voluptates saepe vero iusto veritatis laudantium accusantium, assumenda sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quia enim, et assumenda odit rerum vero pariatur minus commodi iusto soluta architecto porro, cum ducimus id molestias odio vitae voluptates? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, libero numquam dolores temporibus exercitationem a voluptates sit minus perferendis iste consectetur accusamus pariatur tempore cupiditate adipisci labore corporis dolore eaque.";
   const [text, setText] = useState(initialText);
   const ENDPOINT = 'localhost:5000';
   socket = io(ENDPOINT);
@@ -23,23 +23,31 @@ const Content = () => {
   // }, [ENDPOINT]);
 
   socket.on('editingText', function(data){
-    console.log('Client: receiving data: ');
+    console.log('Client: receiving data: '+ data);
     setText(data);
   });
 
   function handleChange(event) {
-    event.preventDefault();
-    console.log('Client sending data: ' + text);
     setText(event.target.value);
+  }
+
+  function setToBold () {
+    document.execCommand ('backColor', false, "green");
+  }
+
+  function applyChange(){
     socket.emit('editingText', text);
   }
 
   return (
     <div className="content-container">
-      <textarea
+      <button onClick={applyChange}>Send</button>
+      <ContentEditable
+        html={text}
         onChange={handleChange}
-        className="content-input"
-        value={text}/>
+        onMouseUp={setToBold}
+        className="content-input">
+      </ContentEditable>
     </div>
   );
 }
