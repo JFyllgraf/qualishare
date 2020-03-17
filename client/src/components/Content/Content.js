40cdd51bdb04f02ac62d09d8e751b893ac1fec6f
Dascham <sydowtwenty@gmail.com>
2020-03-17 18:11:32 +0100

    added handleOnSelect

--- client/src/components/Content/Content.js
+++ client/src/components/Content/Content.js
@@ -7,7 +7,6 @@ import './Content.css';
 import Toolbar from '../Toolbar/Toolbar';
 let socket;
 
-
 function Content({selected, codes, handler}) {
   const initialText = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur assumenda suscipit quos doloribus minus, provident corrupti repudiandae totam ipsam cum numquam! Repellat voluptas magnam amet, labore tempore laborum, dignissimos laudantium?Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatem error, nulla delectus id, nemo aliquam commodi, non distinctio pariatur nisi rem! Provident sapiente, natus assumenda cumque error, esse distinctio porro.Lorem ipsum dolor sit amet, consectetur adipisicing elit. Tempora optio debitis deleniti explicabo repellat quos ipsum itaque doloremque molestiae delectus a voluptates saepe vero iusto veritatis laudantium accusantium, assumenda sunt. Lorem ipsum dolor sit amet, consectetur adipisicing elit. Doloremque, quia enim, et assumenda odit rerum vero pariatur minus commodi iusto soluta architecto porro, cum ducimus id molestias odio vitae voluptates? Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut, libero numquam dolores temporibus exercitationem a voluptates sit minus perferendis iste consectetur accusamus pariatur tempore cupiditate adipisci labore corporis dolore eaque.";
   const [text, setText] = useState(initialText);
@@ -35,6 +34,10 @@ function Content({selected, codes, handler}) {
     socket.emit('editingText', text);
   }
 
+  const handleOnSelect = (e) => {
+    console.log(window.getSelection().anchorOffset);
+  };
+
   return (
     <div className="content-container">
       <Toolbar
@@ -46,6 +49,7 @@ function Content({selected, codes, handler}) {
       <ContentEditable
         html={text}
         onChange={handleChange}
+        onSelect={handleOnSelect}
         className="content-input">
       </ContentEditable>
     </div>
