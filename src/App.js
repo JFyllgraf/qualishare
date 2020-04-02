b48f04a380cd93721adc0e748173878bf82f04bb
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-04-02 16:14:19 +0200

    test

--- src/App.js
+++ src/App.js
@@ -28,7 +28,7 @@ class App extends Component {
         name: "",
         room: "",
         isLoggedIn: false,
-        displayChat: true,
+        displayChat: false,
         codeObjects: [new Code('Political spin'), new Code('Chinese critique'), new Code('Racist remarks')],
         selected: ''
     };
