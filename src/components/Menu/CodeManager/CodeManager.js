75c87b0bc66bb15075984ae24074880c52a1651a
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-04-29 15:38:08 +0200

    Codes are now clickable elements in a list. A module will be opened on click, showing data from state

--- src/components/Menu/CodeManager/CodeManager.js
+++ src/components/Menu/CodeManager/CodeManager.js
@@ -5,142 +5,156 @@ import Code from '../../../data_model/Code'
 import io from "socket.io-client";
 import {server_url} from "../../../Utility/GlobalVariables";
 import axios from 'axios';
-import CustomInput from "reactstrap/es/CustomInput";
+import { ListGroup, ListGroupItem } from 'reactstrap';
+import Modal from 'react-bootstrap/Modal';
 
 let socket;
 const CodeManager = ({addCodeToList, deleteCodeFromList, getCodes, addReceivedCode, userName}) => {
-    const [codename, setcodeName] = useState('');
-    const [onChangeEvent, setonChangeEvent] = useState();
+  const [codename, setcodeName] = useState('');
+  const [onChangeEvent, setonChangeEvent] = useState();
 
-    const ENDPOINT = server_url;
-    socket = io(ENDPOINT);
-    socket.on("newCode", function (data) {
-        let receivedCode = JSON.parse(data);
-        if (!isCodeInList(receivedCode._id)){
-            let newCode = new Code(receivedCode.codeName);
-            newCode._id = receivedCode._id;
-            newCode.color = receivedCode.color;
-            newCode.userName = receivedCode.userName;
-            addReceivedCode(newCode);
-        }
-        //do nothing
-    });
-    socket.on("deleteCode", function(data){
-        let codes = getCodes();
+  const [activeCodeName, setActiveCodeName] = useState();
+  const [activeCodeId, setActiveCodeId] = useState();
+  const [quoteList, setCodeList] = useState();
 
-        for (let i = 0; i < codes.length; i++){
-            if (codes[i].getName() === data){
-                deleteCodeFromList(i);
-            }
-        }
-    });
-    function isCodeInList(id){
-        let codes = getCodes();
-        let bool = false;
+  //modal handling
+  const [show, setShow] = useState(false);
 
-        for (let i = 0; i < codes.length; i++){
-            if (codes[i].getId() === id){
-                bool = true;
-                break;
-            }
-        }
-        return bool;
-    }
-    const handleOnChange = (e) => {
-        e.preventDefault();
-        e.persist();
-        setonChangeEvent(e);
-        setcodeName(e.target.value);
-    };
+  const ENDPOINT = server_url;
+  socket = io(ENDPOINT);
+  socket.on("newCode", function (data) {
+      let receivedCode = JSON.parse(data);
+      if (!isCodeInList(receivedCode._id)){
+          let newCode = new Code(receivedCode.codeName);
+          newCode._id = receivedCode._id;
+          newCode.color = receivedCode.color;
+          newCode.userName = receivedCode.userName;
+          addReceivedCode(newCode);
+      }
+      //do nothing
+  });
+  socket.on("deleteCode", function(data){
+      let codes = getCodes();
 
-    //this adds new code through the button: add new code
-    const handleOnClick = (e) => {
-        e.preventDefault();
-
-        axios.post(server_url+"/newCode", {codeName: codename, userName:userName}).then(res => {
-            let code = constructCodeFromData(res.data);
-            addCodeToList(code);
-        }).catch(err =>{
-            console.log(err);
-        })
-        onChangeEvent.target.value = ''; //reset
-        setonChangeEvent(undefined); //reset
-
-    };
-    function constructCodeFromData(data){
-        let code = new Code(data.codeName, data._id);
-        code.color = data.color
-        code.quoteRefs = data.quoteRefs;
-        code.userName = data.userName;
-        return code;
-    }
-    const handleOnClickDeleteCode = (e) => {
-        e.preventDefault();
-        let codes = getCodes();
-        let codeToDelete = onChangeEvent.target.value;
-
-        for (let i = 0; i < codes.length; i++){
-            if (codes[i].getName() === codeToDelete){
-                axios.delete(server_url+"/deleteCode", {data: codes[i]}).then(res=>{
-                    deleteCodeFromList(i);
-                    onChangeEvent.target.value = '';//reset
-                    setonChangeEvent(undefined)//reset
-                    socket.emit("deleteCode", codeToDelete);
-                }).catch(err=>{
-                    console.log(err);
-                })
-            }
-        }
-    };
-
-
-    function CheckValidInput(e){
-        if(onChangeEvent === undefined){
-            return false;
-        }
-
-        let inputString = onChangeEvent.target.value;
-        if (inputString ===''){
-            return false;
-        }
-
-        let bool = true;
-        let codes = getCodes();
-        for (let i = 0; i < codes.length; i++){
-          if (codes[i].getName() === inputString){
-            bool = false;
+      for (let i = 0; i < codes.length; i++){
+          if (codes[i].getName() === data){
+              deleteCodeFromList(i);
           }
+      }
+  });
+  function isCodeInList(id){
+      let codes = getCodes();
+      let bool = false;
+
+      for (let i = 0; i < codes.length; i++){
+          if (codes[i].getId() === id){
+              bool = true;
+              break;
+          }
+      }
+      return bool;
+  }
+  const handleOnChange = (e) => {
+      e.preventDefault();
+      e.persist();
+      setonChangeEvent(e);
+      setcodeName(e.target.value);
+  };
+
+  //this adds new code through the button: add new code
+  const handleOnClick = (e) => {
+      e.preventDefault();
+
+      axios.post(server_url+"/newCode", {codeName: codename, userName:userName}).then(res => {
+          let code = constructCodeFromData(res.data);
+          addCodeToList(code);
+      }).catch(err =>{
+          console.log(err);
+      })
+      onChangeEvent.target.value = ''; //reset
+      setonChangeEvent(undefined); //reset
+
+  };
+  function constructCodeFromData(data){
+      let code = new Code(data.codeName, data._id);
+      code.color = data.color
+      code.quoteRefs = data.quoteRefs;
+      code.userName = data.userName;
+      return code;
+  }
+  const handleOnClickDeleteCode = (e) => {
+      e.preventDefault();
+      let codes = getCodes();
+      let codeToDelete = onChangeEvent.target.value;
+
+      for (let i = 0; i < codes.length; i++){
+          if (codes[i].getName() === codeToDelete){
+              axios.delete(server_url+"/deleteCode", {data: codes[i]}).then(res=>{
+                  deleteCodeFromList(i);
+                  onChangeEvent.target.value = '';//reset
+                  setonChangeEvent(undefined)//reset
+                  socket.emit("deleteCode", codeToDelete);
+              }).catch(err=>{
+                  console.log(err);
+              })
+          }
+      }
+  };
+
+
+  function CheckValidInput(e){
+      if(onChangeEvent === undefined){
+          return false;
+      }
+
+      let inputString = onChangeEvent.target.value;
+      if (inputString ===''){
+          return false;
+      }
+
+      let bool = true;
+      let codes = getCodes();
+      for (let i = 0; i < codes.length; i++){
+        if (codes[i].getName() === inputString){
+          bool = false;
         }
+      }
 
 
 
-        if(e.target.id === "deletebtn"){
-            bool = !bool;
-        }
-        return bool;
-    }
+      if(e.target.id === "deletebtn"){
+          bool = !bool;
+      }
+      return bool;
+  }
 
-    function numOfQuotes(code) {
-        return "";//" ("+code.quoteRefs.length.toString()+")";
-    }
+  function numOfQuotes(code) {
+      return "";//" ("+code.quoteRefs.length.toString()+")";
+  }
 
-    function DisplayCode() {
-        let codes = getCodes();
-        return (
-            <div className="code-list">
-                {
-                    codes.map(code => {
-                        return (
-                            <div className="code" key={code.getId()}>
-                                <CustomInput type="checkbox" id={code.getId()} label={code.getName()+numOfQuotes(code)}/>
-                            </div>
-                        )
-                    })
-                }
-            </div>
-        )
-    }
+  function DisplayCode() {
+    let codes = getCodes();
+    return (
+      <div className="code-list">
+          {
+            codes.map(code => {
+                return (
+                  <div className="code-element" name={code.getName()} id={code.getId()} key={code.getId()} onClick={openCodeModal}>
+                    {code.getName()}
+                  </div>
+                )
+            })
+          }
+      </div>
+    )
+  }
 
+  function openCodeModal(event){
+    setActiveCodeId(event.target.id);
+    setActiveCodeName(event.target.getAttribute('name'));
+    setShow(true);
+    console.log(event.target.getAttribute('name'));
+  }
 
   return (
     <div className="codeManager-container">
@@ -151,10 +165,31 @@ const CodeManager = ({addCodeToList, deleteCodeFromList, getCodes, addReceivedCo
       </div>
         <div><input type="text" onChange={handleOnChange} onKeyUpCapture={(e) => e.keyCode===13 && CheckValidInput(e) ? handleOnClick(e) : null}/></div>
       <div className="code-list-container">
-        <FormGroup check>
-            {DisplayCode()}
-        </FormGroup>
+        <div className="input-icons">
+          {DisplayCode()}
+        </div>
       </div>
+
+      <Modal
+        show={show}
+        onHide={() => setShow(false)}
+        onEnter={() => console.log('Entering modal')}
+        dialogClassName="custom-modal"
+        aria-labelledby="example-custom-modal-styling-title"
+        centered
+      >
+        <Modal.Header closeButton>
+          <Modal.Title id="example-custom-modal-styling-title">
+            Code: {activeCodeName}
+          </Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>
+            text here
+          </p>
+        </Modal.Body>
+      </Modal>
+
     </div>
   );
 };
