bcbd4df82979c3526f3cf4a9083046c64f6276bb
Jesper Fyllgraf <Jesper92fyllgraf@hotmail.com>
2020-05-04 14:16:03 +0200

    listens to heroku instead of localhost for demo

--- src/Utility/GlobalVariables.js
+++ src/Utility/GlobalVariables.js
@@ -1,4 +1,4 @@
-const server_url = "http://localhost:5000";
-//const server_url = "https://qualishare-server.herokuapp.com/";
+//const server_url = "http://localhost:5000";
+const server_url = "https://qualishare-server.herokuapp.com/";
 
 export { server_url }
